0f47eb1cd1f35de9c8eb028b926e64111b653f0d
Raul Macarie <raul@macarie.me>
2023-12-19 00:30:44 +0100

    fix: missing `defineConfig` in Vitest's config files

--- packages/react-t/vitest.config.ts
+++ packages/react-t/vitest.config.ts
@@ -2,14 +2,14 @@ import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 
 import defaultConfig from "../../vitest.config.ts";
-import { mergeConfig } from "vitest/config";
+import { defineConfig, mergeConfig } from "vitest/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default mergeConfig(
 	defaultConfig,
-	{
+	defineConfig({
 		resolve: {
 			alias: {
 				"~": resolve(__dirname, "source"),
@@ -18,6 +18,6 @@ export default mergeConfig(
 		test: {
 			environment: "happy-dom",
 		},
-	},
+	}),
 	true,
 );
--- packages/t/vitest.config.ts
+++ packages/t/vitest.config.ts
@@ -2,19 +2,19 @@ import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 
 import defaultConfig from "../../vitest.config.ts";
-import { mergeConfig } from "vitest/config";
+import { defineConfig, mergeConfig } from "vitest/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default mergeConfig(
 	defaultConfig,
-	{
+	defineConfig({
 		resolve: {
 			alias: {
 				"~": resolve(__dirname, "source"),
 			},
 		},
-	},
+	}),
 	true,
 );
