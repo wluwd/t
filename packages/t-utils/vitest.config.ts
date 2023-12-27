f6434304c464c7b03f5065de46063e960db1730a
Raul Macarie <raul@macarie.me>
2023-12-27 11:17:30 +0100

    chore(t-utils): create package

---
+++ packages/t-utils/vitest.config.ts
@@ -0,0 +1,20 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import defaultConfig from "../../vitest.config.ts";
+import { defineConfig, mergeConfig } from "vitest/config";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default mergeConfig(
+	defaultConfig,
+	defineConfig({
+		resolve: {
+			alias: {
+				"~": resolve(__dirname, "source"),
+			},
+		},
+	}),
+	true,
+);
