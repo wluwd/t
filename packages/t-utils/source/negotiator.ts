fae07f67bc0caaba1041fe867aaacde350537de1
Raul Macarie <raul@macarie.me>
2024-02-29 22:52:07 +0100

    fix(t-utils): let negotiators infer the return type from usage

--- packages/t-utils/source/negotiator.ts
+++ packages/t-utils/source/negotiator.ts
@@ -1,32 +1,37 @@
 import { filterMatches } from "@fluent/langneg";
 
-export type LocaleNegotiator<Locale> = (
+const typedFilterMatches = filterMatches as <
+	Requested extends string,
+	Available extends string,
+>(
+	requested: readonly Requested[],
+	available: readonly Available[],
+	strategy: "filtering" | "lookup" | "matching",
+) => Available[];
+
+export type LocaleNegotiator<TypeParamLocale extends string = string> = <
+	Locale extends TypeParamLocale,
+>(
 	availableLocales: readonly Locale[],
 ) => Locale | undefined;
 
-export type LocaleNegotiators<Locale> =
+export type LocaleNegotiators<Locale extends string> =
 	| readonly [...(LocaleNegotiator<Locale> | false)[], Locale]
 	| readonly [];
 
-type Algorithm = (
+type Algorithm = <Available extends string = string>(
 	requestedLocales: readonly string[],
-	availableLocales: readonly string[],
-) => string[];
+	availableLocales: readonly Available[],
+) => Available[];
 
 export const lookup: Algorithm = (requestedLocales, availableLocales) =>
-	filterMatches(
-		// @info `filterMatches` expects mutable arrays
-		Array.from(requestedLocales),
-		Array.from(availableLocales),
-		"lookup",
-	);
+	typedFilterMatches(requestedLocales, availableLocales, "lookup");
 
 export const negotiator: (
 	requestedLocales: readonly string[],
 	algorithm: Algorithm,
-) => LocaleNegotiator<string> =
-	(requestedLocales, algorithm) => (availableLocales) =>
-		algorithm(requestedLocales, availableLocales)[0];
+) => LocaleNegotiator = (requestedLocales, algorithm) => (availableLocales) =>
+	algorithm(requestedLocales, availableLocales)[0];
 
 export const browser = () =>
 	negotiator(globalThis.navigator?.languages ?? [], lookup);
